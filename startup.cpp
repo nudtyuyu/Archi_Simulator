17b7c99e58014a947e768e63a0cdcc866b335a92
Rhein_E <1308498716@qq.com>
2022-10-28 08:43:32 +0800

    rename

---
+++ startup.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#define BUFSIZE 256
+typedef unsigned int uint;
+
+void load(char *textfile, char *datafile, uint taddr, uint daddr) {
+    char *buf[BUFSIZE];
+    FILE *fin = NULL;
+    uint addr;
+    int cnt;
+
+    // load .text
+    fin = fopen(textfile, "rb");
+    addr = taddr;
+    while (cnt = fread(buf, 1, BUFSIZE, fin)) {
+        // MMU write(addr, cnt, buf)
+        addr += cnt;
+    }
+    fclose(fin);
+
+    // load .data
+    fin = fopen(datafile, "rb");
+    addr = daddr;
+    while (cnt = fread(buf, 1, BUFSIZE, fin)) {
+        // MMU write(addr, cnt, buf)
+        addr += cnt;
+    }
+    fclose(fin);
+
+    return;
+}
+
+void startup() {
+    load("text", "data", 0x00010000, 0x10004000);
+    // cpu.pc = 0x00010000
+    // cpu.run()
+}
